d2ffbcb2c458f1dd6e9dca9a628ce4539a292f89
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-05-15 13:20:35 -0400

    -API testing

--- src/components/LocTemp/LocTemp.jsx
+++ src/components/LocTemp/LocTemp.jsx
@@ -1,7 +1,19 @@
 import { useEffect } from 'react';
 
-export default function LocTemp() { 
-    useEffect(() => { 
+const BASEURL = 'https://api.openweathermap.org/data/3.0/';
+
+export default function LocTemp() {
+    /* const [currTemp, setCurrTemp] = useState(); */
+
+    let lat = 17.6;
+    let lon = 55.9;
+    const API_KEY = '69783222903c34d31614d443203ff766';
+
+    useEffect(() => {
+        fetch('http://api.dictionaryapi.dev/api/v2/entries/en/hello')
+            /* `${BASEURL}onecall?lat=${lat}&lon=${lon}&exclude={minutely,hoursly,daily,alerts}&units={imperial}&appid=${API_KEY}` */
+            .then(response => response.json())
+            .then(json => console.log(json));
         console.log('page loaded');
      }, []);
     return '999';    
