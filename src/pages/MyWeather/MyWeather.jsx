893942c595f3488cb98accdaf1d7d3198cbe7835
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-05-15 14:14:35 -0400

    -Add geolocation call in loc-service

--- src/pages/MyWeather/MyWeather.jsx
+++ src/pages/MyWeather/MyWeather.jsx
@@ -2,13 +2,23 @@ import { useState, useEffect } from 'react';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import LocTemp from '../../components/LocTemp/LocTemp';
+import * as locService from '../../utilities/loc-service';
 
 export default function MyWeather({ setUser }) {
   const [coords,setCoords] = useState(null);
+  const [msg,setMsg] = useState('');
   const [showSignUp, setShowSignUp] = useState(false);
 
   useEffect(() => {
-
+    async function getCoords() {
+      try {
+        const coords = await locService.getLocation();
+        setCoords(coords);
+      } catch {
+        setMsg('No go for location.');
+      }
+    }
+    getCoords();
   },[]);
 
   return (
@@ -27,7 +37,9 @@ export default function MyWeather({ setUser }) {
               </div>
             </div>
           </div>
-        </div>
+      </div>
+      <h3>&nbsp;{msg}</h3>
+      <p>{JSON.stringify(coords)}</p>
     </main>
   );
 }
