0b9a7ea6f8a82d7ae82f26485998ec69614640ea
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-05-17 17:57:19 -0400

    -Added favicon and nav link styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,17 +12,17 @@ export default function NavBar({ user, setUser }) {
     <div className="container-fluid">
       <div className="navbar-header">    
         <section className="navbar-brand">
-            <Link to="/">myWeather</Link>
+            <Link className="homeLink" to="/">myWeather</Link>
           </section>
       </div>
     <ul className="nav navbar-nav">
-      <li className="nav nav-bar navbar-center">{new Date().toLocaleString()}</li>
+      <li className="nav nav-bar navbar-center navDate">{new Date().toLocaleString()}</li>
     </ul>
     <ul className="nav navbar-nav navbar-right">
         { user ?
-          <li className="glyphicon glyphicon-log-out">< Link to="/" onClick={handleLogOut}>Log Out</Link></li>
+          <li className="inOutLink">< Link to="/" onClick={handleLogOut}>Log Out</Link></li>
           :
-          <li className="glyphicon glyphicon-log-in">< Link to="/AuthPage">Log In</Link></li>}
+          <li className="inOutLink">< Link to="/AuthPage">Log In</Link></li>}
     </ul>
   </div>
 </nav>
