7c96c5bc4a56ddda9c2d13a0b16ab76a5119e580
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-06-26 11:20:29 -0400

    Add live time updates to navbar: -Implemented client-side timer that updates every minute -Shortened format to XX:XX (no seconds), no additional API calls required

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import * as userService from '../../utilities/users-service';
 
 export default function NavBar({ user, setUser }) {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    // Update time immediately
+    setCurrentTime(new Date());
+    
+    // Set up interval to update time every minute (60000ms)
+    const timeInterval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 60000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(timeInterval);
+  }, []);
+
   function handleLogOut() {
     userService.logOut();
     setUser(null);
-}
+  }
 
   return (
 <nav className="navbar navbar-inverse navBar">
@@ -18,7 +34,13 @@ export default function NavBar({ user, setUser }) {
           </section>
       </div>
     <ul className="nav navbar-nav">
-      <li className="nav nav-bar navbar-center navDate">{new Date().toLocaleString()}</li>
+      <li className="nav nav-bar navbar-center navDate">{currentTime.toLocaleString('en-US', { 
+        month: 'short', 
+        day: 'numeric', 
+        year: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit' 
+      })}</li>
     </ul>
     <ul className="nav navbar-nav navbar-right">
         { user ?
